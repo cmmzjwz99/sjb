c4676af5c487ee6f931d5b637e7ff3ae21359864
boom7l <13868323606@163.com>
2017-12-18 16:44:29 +0800

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,21 +12,123 @@
 
 ActiveRecord::Schema.define(version: 2017082414512011) do
 
-  create_table "car_messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "status", default: "unverified"
+  create_table "basic_messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "status", default: "verifyfail"
     t.integer "verify_user"
     t.bigint "loan_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "company"
+    t.string "dkxs"
+    t.string "dklx"
+    t.string "dkqx"
+    t.string "dbfs"
+    t.string "dblx"
+    t.string "hkfs"
+    t.string "dkcp"
+    t.string "dkje"
+    t.string "lsed"
+    t.string "zjkje"
+    t.string "hkzl"
+    t.string "jkyt"
+    t.string "hkly"
+    t.string "dkqxdw"
+    t.datetime "ksrq"
+    t.datetime "jsrq"
+    t.string "zdr"
+    t.datetime "zdrq"
+    t.string "sqb"
+    t.string "clxxb"
+    t.string "cljcb"
+    t.string "yybspb"
+    t.string "csb"
+    t.index ["loan_id"], name: "index_basic_messages_on_loan_id"
+  end
+
+  create_table "car_messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "status", default: "verifyfail"
+    t.integer "verify_user"
+    t.bigint "loan_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "no"
+    t.string "name"
+    t.datetime "pgrq"
+    t.string "pgjz"
+    t.datetime "qsr"
+    t.datetime "zzr"
+    t.string "cfdz"
+    t.string "ypbz"
+    t.string "ypdw"
+    t.float "zdyl", limit: 24
+    t.float "kdjz", limit: 24
+    t.float "yyjz", limit: 24
+    t.integer "ypsl"
+    t.string "dq"
+    t.string "pgdw"
+    t.string "pgry"
+    t.integer "ydgps"
+    t.integer "gdgps"
+    t.string "clsbm"
+    t.string "qcpp"
+    t.string "qcxh"
+    t.string "qbxhxx"
+    t.string "csbs"
+    t.datetime "spsj"
+    t.datetime "ccrq"
+    t.string "xsgl"
+    t.string "clys"
+    t.string "nsqk"
+    t.string "qmqk"
+    t.string "gkzk"
+    t.string "ghcs"
+    t.float "xtpgj", limit: 24
+    t.float "pjpgj", limit: 24
+    t.string "qcpz"
+    t.string "fkfs"
+    t.string "fdjhm"
+    t.string "gcjk"
+    t.datetime "gmsj"
+    t.string "gmjg"
+    t.string "synx"
+    t.string "pl"
     t.index ["loan_id"], name: "index_car_messages_on_loan_id"
   end
 
+  create_table "customer_images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "img"
+    t.string "style"
+    t.bigint "customer_message_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_message_id"], name: "index_customer_images_on_customer_message_id"
+  end
+
   create_table "customer_messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "status", default: "unverified"
+    t.string "status", default: "verifyfail"
     t.integer "verify_user"
     t.bigint "loan_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "company"
+    t.string "dkxs"
+    t.string "dklx"
+    t.string "dkqx"
+    t.string "dbfs"
+    t.string "dblx"
+    t.string "hkfs"
+    t.string "dkcp"
+    t.string "dkje"
+    t.string "lsed"
+    t.string "zjkje"
+    t.string "hkzl"
+    t.string "jkyt"
+    t.string "hkly"
+    t.string "dkqxdw"
+    t.datetime "ksrq"
+    t.datetime "jsrq"
+    t.string "zdr"
+    t.datetime "zdrq"
     t.index ["loan_id"], name: "index_customer_messages_on_loan_id"
   end
 
@@ -37,11 +139,20 @@ ActiveRecord::Schema.define(version: 2017082414512011) do
     t.bigint "loan_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "overdue_time"
+    t.float "gpsfy", limit: 24, default: 0.0
+    t.float "gpsllf", limit: 24, default: 0.0
+    t.float "jjf", limit: 24, default: 0.0
+    t.float "zjfd", limit: 24, default: 0.0
+    t.float "fwf", limit: 24, default: 0.0
+    t.float "bj", limit: 24, default: 0.0
+    t.float "lx", limit: 24, default: 0.0
+    t.float "dkye", limit: 24, default: 0.0
     t.index ["loan_id"], name: "index_instalments_on_loan_id"
   end
 
   create_table "loan_comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "status", default: "unverified"
+    t.string "status", default: "verifyfail"
     t.integer "verify_user"
     t.bigint "loan_id"
     t.datetime "created_at", null: false
@@ -49,11 +160,20 @@ ActiveRecord::Schema.define(version: 2017082414512011) do
     t.index ["loan_id"], name: "index_loan_comments_on_loan_id"
   end
 
+  create_table "loan_images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "img"
+    t.string "style"
+    t.bigint "loan_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["loan_id"], name: "index_loan_images_on_loan_id"
+  end
+
   create_table "loans", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "first_verify", default: "unverified"
+    t.string "first_verify", default: "verifyfail"
     t.integer "verify_user"
-    t.string "customer_verify", default: "unverified"
-    t.string "car_verify", default: "unverified"
+    t.string "customer_verify", default: "verifyfail"
+    t.string "car_verify", default: "verifyfail"
     t.boolean "has_pay", default: false
     t.bigint "user_id"
     t.datetime "created_at", null: false
@@ -62,6 +182,8 @@ ActiveRecord::Schema.define(version: 2017082414512011) do
     t.string "name"
     t.string "phone"
     t.string "source"
+    t.string "location"
+    t.string "basic_verify", default: "verifyfail"
     t.index ["user_id"], name: "index_loans_on_user_id"
   end
 
@@ -69,6 +191,33 @@ ActiveRecord::Schema.define(version: 2017082414512011) do
     t.string "label"
     t.string "nicename"
     t.text "modules"
+  end
+
+  create_table "user_areas", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.boolean "zjwz", default: false
+    t.boolean "zjsxsz", default: false
+    t.boolean "zjsxkq", default: false
+    t.boolean "zjsxyc", default: false
+    t.boolean "zjhz", default: false
+    t.boolean "ahhf", default: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_areas_on_user_id"
+  end
+
+  create_table "user_powers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.boolean "input", default: false
+    t.boolean "first_verify", default: false
+    t.boolean "online_verify", default: false
+    t.boolean "customer_verify", default: false
+    t.boolean "car_verify", default: false
+    t.boolean "user_manage", default: false
+    t.boolean "financial", default: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_powers_on_user_id"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -104,9 +253,14 @@ ActiveRecord::Schema.define(version: 2017082414512011) do
     t.index ["profile_id"], name: "USER_PROFILE_ID"
   end
 
+  add_foreign_key "basic_messages", "loans"
   add_foreign_key "car_messages", "loans"
+  add_foreign_key "customer_images", "customer_messages"
   add_foreign_key "customer_messages", "loans"
   add_foreign_key "instalments", "loans"
   add_foreign_key "loan_comments", "loans"
+  add_foreign_key "loan_images", "loans"
   add_foreign_key "loans", "users"
+  add_foreign_key "user_areas", "users"
+  add_foreign_key "user_powers", "users"
 end
