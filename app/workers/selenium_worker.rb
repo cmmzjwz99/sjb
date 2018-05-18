9241a031cc0d4eac119ea184fc0371ed76d0c05c
qiqidone <qiqidone@gmail.com>
2018-05-18 14:02:07 +0800

    update spider

--- app/workers/selenium_worker.rb
+++ app/workers/selenium_worker.rb
@@ -4,49 +4,58 @@ class SeleniumWorker
   include Sidekiq::Worker
 
   def perform
-    options = Selenium::WebDriver::Chrome::Options.new
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--disable-popup-blocking')
-    options.add_argument('--disable-translate')
-    options.add_argument('--headless')
-    driver = Selenium::WebDriver.for :chrome, options: options
-    base_url = 'http://vip.win007.com/AsianOdds_n.aspx?id=1482830'
-    driver.get(base_url)
-    button = driver.find_element(:xpath, '//*[@id="odds"]/tbody/tr[12]/td[2]/span')
-    button.click()
+    spider()
+  end
 
-    teams = driver.find_element(:xpath, '//*[@id="webmain"]/table[1]/tbody/tr[2]/td')
-    p teams.text.split('vs').split(' ')
-    home = '葡萄牙'
-    away = '西班牙'
-    source = 'bet365'
-    while(true)
-      element_home = driver.find_element(:xpath, '//*[@id="odds"]/tbody/tr[12]/td[6]')
-      element_play = driver.find_element(:xpath, '//*[@id="odds"]/tbody/tr[12]/td[7]')
-      element_away = driver.find_element(:xpath, '//*[@id="odds"]/tbody/tr[12]/td[8]')
-      p 'Bet365即时盘口1：', element_home.text, element_play.text, element_away.text
+  def spider
+    begin
+      options = Selenium::WebDriver::Chrome::Options.new
+      options.add_argument('--ignore-certificate-errors')
+      options.add_argument('--disable-popup-blocking')
+      options.add_argument('--disable-translate')
+      options.add_argument('--headless')
+      driver = Selenium::WebDriver.for :chrome, options: options
+      base_url = 'http://vip.win007.com/AsianOdds_n.aspx?id=1482830'
+      driver.get(base_url)
+      button = driver.find_element(:xpath, '//*[@id="odds"]/tbody/tr[12]/td[2]/span')
+      button.click()
 
-      save_if_updated(home, away, element_play.text, element_home.text.to_f, element_away.text.to_f, source)
+      teams = driver.find_element(:xpath, '//*[@id="webmain"]/table[1]/tbody/tr[2]/td')
+      p teams.text.split('vs').split(' ')
+      home = '葡萄牙'
+      away = '西班牙'
+      source = 'bet365'
+      while(true)
+        element_home = driver.find_element(:xpath, '//*[@id="odds"]/tbody/tr[12]/td[6]')
+        element_play = driver.find_element(:xpath, '//*[@id="odds"]/tbody/tr[12]/td[7]')
+        element_away = driver.find_element(:xpath, '//*[@id="odds"]/tbody/tr[12]/td[8]')
+        p 'Bet365即时盘口1：', element_home.text, element_play.text, element_away.text
 
-      element_home = driver.find_element(:xpath, '//*[@id="odds"]/tbody/tr[13]/td[6]')
-      element_play = driver.find_element(:xpath, '//*[@id="odds"]/tbody/tr[13]/td[7]')
-      element_away = driver.find_element(:xpath, '//*[@id="odds"]/tbody/tr[13]/td[8]')
-      p 'Bet365即时盘口2：', element_home.text, element_play.text, element_away.text
+        save_if_updated(home, away, element_play.text, element_home.text.to_f, element_away.text.to_f, source)
 
-      save_if_updated(home, away, element_play.text, element_home.text.to_f, element_away.text.to_f, source)
+        element_home = driver.find_element(:xpath, '//*[@id="odds"]/tbody/tr[13]/td[6]')
+        element_play = driver.find_element(:xpath, '//*[@id="odds"]/tbody/tr[13]/td[7]')
+        element_away = driver.find_element(:xpath, '//*[@id="odds"]/tbody/tr[13]/td[8]')
+        p 'Bet365即时盘口2：', element_home.text, element_play.text, element_away.text
 
-      element_home = driver.find_element(:xpath, '//*[@id="odds"]/tbody/tr[14]/td[6]')
-      element_play = driver.find_element(:xpath, '//*[@id="odds"]/tbody/tr[14]/td[7]')
-      element_away = driver.find_element(:xpath, '//*[@id="odds"]/tbody/tr[14]/td[8]')
-      p 'Bet365即时盘口3：', element_home.text, element_play.text, element_away.text
+        save_if_updated(home, away, element_play.text, element_home.text.to_f, element_away.text.to_f, source)
 
-      save_if_updated(home, away, element_play.text, element_home.text.to_f, element_away.text.to_f, source)
+        element_home = driver.find_element(:xpath, '//*[@id="odds"]/tbody/tr[14]/td[6]')
+        element_play = driver.find_element(:xpath, '//*[@id="odds"]/tbody/tr[14]/td[7]')
+        element_away = driver.find_element(:xpath, '//*[@id="odds"]/tbody/tr[14]/td[8]')
+        p 'Bet365即时盘口3：', element_home.text, element_play.text, element_away.text
 
-      sleep(60)
-      driver.navigate().refresh
+        save_if_updated(home, away, element_play.text, element_home.text.to_f, element_away.text.to_f, source)
+
+        driver.navigate().refresh
+        sleep(60)
+      end
+    rescue Exception => e
+      puts e.message
+      puts e.backtrace.inspect
+      driver.close
+      spider()
     end
-
-    driver.close
   end
 
   def save_if_updated(home,away,rule,home_odd, away_odd,source)
