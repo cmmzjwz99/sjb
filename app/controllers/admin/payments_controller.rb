720880035cfd85c367aacc0d8ddc5d4032e81951
13868323606@163.com <13868323606@163.com>
2018-05-21 11:50:32 +0800

    update admin payments

---
+++ app/controllers/admin/payments_controller.rb
@@ -0,0 +1,44 @@
+class Admin::PaymentsController < Admin::BaseController
+
+  def index
+    conditions = {}
+
+    params[:login].present? &&
+        conditions.merge!({user_id: params[:login]})
+
+    @payments = Payment.where(conditions).page(params[:page]).per(10)
+  end
+
+  def sh_fail
+    @payment = Payment.find(params[:id])
+    @payment.status=2
+    @payment.save
+    respond_to do |format|
+      format.html { redirect_to admin_payments_path, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  def sh_success
+    @payment = Payment.find(params[:id])
+    @payment.status=1
+    @payment.pay
+    @payment.save
+    respond_to do |format|
+      format.html { redirect_to admin_payments_path, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+
+
+  def destroy
+    @payment = Payment.find(params[:id])
+    @payment.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_payments_path, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+end
--- app/controllers/api/payments_controller.rb
+++ app/controllers/api/payments_controller.rb
@@ -4,11 +4,17 @@ class Api::PaymentsController < Api::BaseController
     @payment.payment_type=true
     @payment.user=current_user
     @payment.status=0
+
     if @payment.save
-      render json: {code:0,msg:'成功'}
-      return
+      if @payment.balance == 0
+        render json: {code:1,msg:'金额不能为0'}
+        return
+      elsif @payment.balance > 100000
+        render json: {code:1,msg:'金额不能超过100000'}
+        return
+      end
     else
-      render json: {code:1,msg:'失败'}
+      render json: {code:0,msg:'成功'}
       return
     end
   end
