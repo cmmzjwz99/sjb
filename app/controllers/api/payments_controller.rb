cc23c004d20e6de307adedfec467d07fc9a7c47a
boom7l <13868323606@163.com>
2018-05-24 17:59:11 +0800

    update payment

--- app/controllers/api/payments_controller.rb
+++ app/controllers/api/payments_controller.rb
@@ -39,29 +39,32 @@ class Api::PaymentsController < Api::BaseController
       if Payment.find_by(user: current_user, payment_type: 0).status == 0
         return(render json: {code: 1, msg: '您已经有一笔待审核'})
       end
-    else
-      @payment = Payment.new(payment_params)
-      @payment.payment_type=0
-      @payment.user=current_user
-      @payment.status=0
+    end
 
-      if @payment.balance == 0
-        render json: {code: 1, msg: '金额不能为0'}
-        return
-      elsif @payment.balance > 100000
-        render json: {code: 1, msg: '金额不能超过100000'}
-        return
-      elsif @payment.balance > current_user.points
-        render json: {code: 1, msg: '提现金额不能大于余额'}
-        return
-      end
-      user=current_user
-      user-=@payment.balance
+    @payment = Payment.new(payment_params)
+    @payment.payment_type=0
+    @payment.user=current_user
+    @payment.status=0
+
+    if @payment.balance == 0
+      render json: {code: 1, msg: '金额不能为0'}
+      return
+    elsif @payment.balance > 100000
+      render json: {code: 1, msg: '金额不能超过100000'}
+      return
+    elsif @payment.balance > current_user.points
+      render json: {code: 1, msg: '提现金额不能大于余额'}
+      return
+    end
+    user=current_user
+    user-=@payment.balance
+    if @payment.save
       user.save
-      if @payment.save
-        render json: {code: 0, msg: '成功'}
-        return
-      end
+      render json: {code: 0, msg: '成功'}
+      return
+    else
+      render json:{code:1,msg:'错误'}
+      return
     end
   end
 
