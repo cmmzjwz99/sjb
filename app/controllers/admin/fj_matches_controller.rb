70b3b37e43283ef89941176925c1f97f90294ba8
13868323606@163.com <13868323606@163.com>
2018-05-28 14:19:00 +0800

    update fj_matches add search

--- app/controllers/admin/fj_matches_controller.rb
+++ app/controllers/admin/fj_matches_controller.rb
@@ -2,9 +2,20 @@ class Admin::FjMatchesController <  Admin::BaseController
   def index
     sql=''
 
-
+    # params[:team1].present? &&
+    #     conditions.merge!({team1: params[:team1]})
+    # params[:team2].present? &&
+    #     conditions.merge!({team2: params[:team2]})
+    # params[:date].present? &&
+    #     conditions.merge!({time:DateTime.parse(params[:date]).all_day})
     params[:name].present? &&
         sql="name like '%#{params[:name]}%'"
+    params[:team1].present? &&
+        sql="team1 like '%#{params[:team1]}%'"
+    params[:team2].present? &&
+        sql="team2 like '%#{params[:team2]}%'"
+    params[:date].present? &&
+        sql="time like '%#{params[:date]}%'"
 
     @matches=FjMatch.where(' match_id NOT IN (SELECT match_id FROM matchs where match_id is not null)')
                     .where(sql).order(:time).page(params[:page]).per(10)
