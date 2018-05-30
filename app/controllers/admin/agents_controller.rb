670120fae8c7f64f078ae4329e01cf65c9352cfd
13868323606@163.com <13868323606@163.com>
2018-05-30 11:42:09 +0800

    update admin 代理页面

--- app/controllers/admin/agents_controller.rb
+++ app/controllers/admin/agents_controller.rb
@@ -1,15 +1,22 @@
-class Admin::AgentsController <  Admin::BaseController
+class Admin::AgentsController < Admin::BaseController
+  before_action :set_user ,only: [:show,:settlement,:update]
   def index
-    conditions={profile_id:2}
+    conditions={profile_id: 2}
     #conditions={}
     params[:name].present? &&
-      conditions.merge!({login:params[:name]})
+        conditions.merge!({login: params[:name]})
 
     @users=Agent.where(conditions).page(params[:page]).per(10)
   end
 
   def new
     @user=Agent.new()
+  end
+
+  def show
+    @user
+    conditions = {user: User.where(father_id: @user.id)}
+    @payments = Payment.where(conditions).page(params[:page]).per(10)
   end
 
   def create
@@ -32,6 +39,7 @@ class Admin::AgentsController <  Admin::BaseController
   def set_user
     @user = Agent.find(params[:id])
   end
+
   def user_params
     params.require(:user).permit!
   end
