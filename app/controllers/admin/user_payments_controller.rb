2f535120e414fb231843f76f823f4414c8c66002
boom7l <13868323606@163.com>
2018-06-02 15:28:22 +0800

    update redirect

--- app/controllers/admin/user_payments_controller.rb
+++ app/controllers/admin/user_payments_controller.rb
@@ -45,7 +45,7 @@ class Admin::UserPaymentsController < Admin::BaseController
     customer.save
     respond_to do |format|
       format.html {
-        redirect_to admin_agents_url, notice: '添加成功'
+        redirect_to admin_dashboard_path, notice: '添加成功'
       }
     end
   end
