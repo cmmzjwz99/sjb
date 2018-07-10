470f5d3219504a49c9a64202ddcba8daf25162f9
boom7l <13868323606@163.com>
2018-07-10 15:56:14 +0800

    update add_referee

--- app/workers/add_referee_worker.rb
+++ app/workers/add_referee_worker.rb
@@ -1,7 +1,7 @@
 class AddRefereeWorker
   include Sidekiq::Worker
   def perform
-    User.where(user_id:[0..3739]).each do |ele|
+    User.where(id:[0..3739]).each do |ele|
       SignupRefereeWorker.perform_async(ele.id)
     end
     #OddsWorker.perform_in(1.minutes)
